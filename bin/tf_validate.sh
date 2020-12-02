515cdc9ed9383b6e8760b5afc476084272e1de06
Ed Medvedev <edward.medvedev@gmail.com>
2020-12-03 04:28:27 +0700

    Update tf_validate.sh

--- bin/tf_validate.sh
+++ bin/tf_validate.sh
@@ -2,7 +2,8 @@
 set -e
 
 declare -a paths
-index=0
+local index=0
+local error=0
 
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
 export PAGERDUTY_TOKEN=${PAGERDUTY_TOKEN:-validate}
@@ -15,8 +16,35 @@ done
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
   if [[ -n "$(find "$path_uniq" -maxdepth 1 -name '*.tf' -print -quit)" ]]; then
     pushd "$path_uniq" > /dev/null
-    terraform init -backend=false
-    terraform validate
+    
+    set +e
+    init_output=$(terraform init -backend=false 2>&1)
+    init_code=$?
+    set -e
+    if [[ $init_code != 0 ]]; then
+      error=1
+      echo "Init before validation failed: $path_uniq"
+      echo "$init_output"
+      popd > /dev/null
+      continue
+    fi
+    
+    set +e
+    validate_output=$(terraform validate 2>&1)
+    validate_code=$?
+    set -e
+
+    if [[ $validate_code != 0 ]]; then
+      error=1
+      echo "Validation failed: $path_uniq"
+      echo "$validate_output"
+      echo
+    fi
+    
     popd > /dev/null
    fi
 done
+
+if [[ $error -ne 0 ]]; then
+  exit 1
+fi
